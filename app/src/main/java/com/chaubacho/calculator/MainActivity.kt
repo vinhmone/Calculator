90044cbcbaf99e71363c72aab9495a74806a252d
Vinh Tran <vinhmone@gmail.com>
2021-06-17 14:45:05 +0700

    ver2

--- app/src/main/java/com/chaubacho/calculator/MainActivity.kt
+++ app/src/main/java/com/chaubacho/calculator/MainActivity.kt
@@ -1,103 +1,114 @@
 package com.chaubacho.calculator
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import com.chaubacho.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private val STATE_PENDING_OPERATION = "PendingOperation"
-    private val STATE_OPERAND = "Operand1"
     private var numberButtons = listOf<Button>()
     private var operationButtons = listOf<Button>()
-
-    private var currentOperand: Double? = null
-    private var pendingOperation: String? = getString(R.string.operation_equal)
+    private var currentOperand = Double.MAX_VALUE
+    private var pendingOperation = ""
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         initViews()
+        initData()
     }
 
-    private fun initViews() {
-        numberButtons = listOf(button0, button1, button2, button3, button4,
-            button5, button6, button7, button8, button9, buttonDot)
-        operationButtons = listOf(buttonEqual, buttonPlus, buttonMinus,
-            buttonMultiply, buttonDivide)
-        numberButtons.forEach { it.setOnClickListener(this) }
-        operationButtons.forEach { it.setOnClickListener(this) }
-        buttonNeg.setOnClickListener(this)
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(STATE_PENDING_OPERATION, pendingOperation)
+        outState.putDouble(STATE_OPERAND, currentOperand)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        pendingOperation = savedInstanceState.getString(STATE_PENDING_OPERATION).toString()
+        currentOperand = savedInstanceState.getDouble(STATE_OPERAND)
+        binding.textOperation.text = pendingOperation
     }
 
     override fun onClick(v: View?) {
-        when (v) {
-            in numberButtons -> writeNumbers((v as Button).text.toString())
-            in operationButtons -> writeOperation((v as Button).text.toString())
-            buttonNeg -> writeNegation()
+        if (v is Button) {
+            when (v) {
+                in numberButtons -> writeNumbers(v.text.toString())
+                in operationButtons -> writeOperation(v.text.toString())
+                binding.buttonNeg -> writeNegation()
+            }
         }
     }
 
+    private fun initViews() {
+        with(binding) {
+            numberButtons = listOf(button0, button1, button2, button3, button4, button5,
+                button6, button7, button8, button9, buttonDot)
+            operationButtons = listOf(buttonEqual, buttonPlus, buttonMinus, buttonMultiply,
+                buttonDivide)
+        }
+        numberButtons.forEach { it.setOnClickListener(this) }
+        operationButtons.forEach { it.setOnClickListener(this) }
+        binding.buttonNeg.setOnClickListener(this)
+    }
+
+    private fun initData() {
+        pendingOperation = getString(R.string.operation_equal)
+    }
+
     private fun writeNumbers(s: String) {
-        editTextNewNumber.append(s)
+        binding.editTextNewNumber.append(s)
     }
 
     private fun writeOperation(op: String) {
-        val value = editTextNewNumber.text.toString()
+        val value = binding.editTextNewNumber.text.toString()
         if (value.isNotEmpty()) {
             value.toDoubleOrNull()?.let {
                 performOperation(it, op)
             }
         }
         pendingOperation = op
-        textOperation.text = pendingOperation
+        binding.textOperation.text = pendingOperation
     }
 
     private fun writeNegation() {
-        val value = editTextNewNumber.text.toString()
-        if (value.isEmpty()) editTextNewNumber.setText(getString(R.string.operation_minus))
-        else {
+        val value = binding.editTextNewNumber.text.toString()
+        if (value.isEmpty()) {
+            binding.editTextNewNumber.setText(getString(R.string.operation_minus))
+        } else {
             value.toDoubleOrNull()?.let {
-                editTextNewNumber.setText("${it * -1}")
-            } ?: editTextNewNumber.setText("")
+                binding.editTextNewNumber.setText("${it * -1}")
+            } ?: binding.editTextNewNumber.setText("")
         }
     }
 
     private fun performOperation(value: Double, op1: String) {
-        if (currentOperand == null) {
+        if (currentOperand == Double.MAX_VALUE) {
             currentOperand = value
         } else {
-            if (pendingOperation.equals(getString(R.string.operation_equal))
-                && textOperation.text != getString(R.string.operation_equal))
-                pendingOperation = op1
+            if (pendingOperation == getString(R.string.operation_equal)
+                && binding.textOperation.text != getString(R.string.operation_equal)
+            ) pendingOperation = op1
             currentOperand = when (pendingOperation) {
                 getString(R.string.operation_equal) -> value
-                getString(R.string.operation_plus) -> currentOperand?.plus(value)
-                getString(R.string.operation_minus) -> currentOperand?.minus(value)
-                getString(R.string.operation_times) -> currentOperand?.times(value)
-                getString(R.string.operation_divide) -> currentOperand?.div(value)
+                getString(R.string.operation_plus) -> currentOperand.plus(value)
+                getString(R.string.operation_minus) -> currentOperand.minus(value)
+                getString(R.string.operation_times) -> currentOperand.times(value)
+                getString(R.string.operation_divide) -> currentOperand.div(value)
                 else -> return
             }
         }
-        editTextResult.setText("$currentOperand")
-        editTextNewNumber.setText("")
+        binding.editTextResult.setText(currentOperand.toString())
+        binding.editTextNewNumber.setText("")
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        outState.putString(STATE_PENDING_OPERATION, pendingOperation)
-        currentOperand?.let {
-            outState.putDouble(STATE_OPERAND, currentOperand!!)
-        }
-        super.onSaveInstanceState(outState)
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        pendingOperation = savedInstanceState.getString(STATE_PENDING_OPERATION)
-        currentOperand = savedInstanceState.getDouble(STATE_OPERAND);
-        textOperation.text = pendingOperation
+    companion object {
+        const val STATE_PENDING_OPERATION = "PendingOperation"
+        const val STATE_OPERAND = "Operand"
     }
 }
